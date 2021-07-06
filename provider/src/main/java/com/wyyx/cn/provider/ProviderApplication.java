ff4dfc3d2cd3b25e6442afd6aff3144f080cdd29
youjief <919175376@qq.com>
2021-07-06 11:31:51 +0800

    eureka 启动

--- provider/src/main/java/com/wyyx/cn/provider/ProviderApplication.java
+++ provider/src/main/java/com/wyyx/cn/provider/ProviderApplication.java
@@ -2,7 +2,10 @@ package com.wyyx.cn.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+//开启往注册中心注册
+@EnableEurekaClient
 @SpringBootApplication
 public class ProviderApplication {
 
